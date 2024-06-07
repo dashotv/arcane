1f9783d66049199ba14eb9160e865a2dadc4adbb
Shawn Catanzarite <me@shawncatz.com>
2024-06-07 04:20:47 +0000

    copy tower's library functions

--- internal/app/models_library.go
+++ internal/app/models_library.go
@@ -1,5 +1,7 @@
 package app
 
+import "github.com/dashotv/fae"
+
 func (c *Connector) LibraryGet(id string) (*Library, error) {
 	m := &Library{}
 	err := c.Library.Find(id, m)
@@ -7,9 +9,25 @@ func (c *Connector) LibraryGet(id string) (*Library, error) {
 		return nil, err
 	}
 
-	// post process here
+	c.processLibraries([]*Library{m})
 
 	return m, nil
+}
+
+func (c *Connector) LibraryGetByKind(kind string) (*Library, error) {
+	list, err := c.Library.Query().Where("kind", kind).Run()
+	if err != nil {
+		return nil, err
+	}
+	if len(list) == 0 {
+		return nil, fae.Errorf("library not found for kind: %s", kind)
+	}
+	if len(list) > 1 {
+		return nil, fae.Errorf("multiple libraries found for kind: %s", kind)
+	}
+
+	c.processLibraries(list)
+	return list[0], nil
 }
 
 func (c *Connector) LibraryList(page, limit int) ([]*Library, error) {
@@ -19,5 +37,31 @@ func (c *Connector) LibraryList(page, limit int) ([]*Library, error) {
 		return nil, err
 	}
 
+	c.processLibraries(list)
 	return list, nil
 }
+
+func (c *Connector) processLibraries(list []*Library) {
+	for _, l := range list {
+		dt, err := c.LibraryTemplateGet(l.LibraryTemplateID.Hex())
+		if err != nil {
+			continue
+		}
+		l.LibraryTemplate = dt
+
+		rt, err := c.LibraryTypeGet(l.LibraryTypeID.Hex())
+		if err != nil {
+			continue
+		}
+		l.LibraryType = rt
+	}
+}
+
+// func (c *Connector) LibraryDestination(m *Medium) (string, error) {
+// 	lib, err := app.DB.LibraryGetByKind(string(m.Kind))
+// 	if err != nil {
+// 		return "", fae.Wrap(err, "failed to get library")
+// 	}
+//
+// 	return lib.LibraryTemplate.Name, nil
+// }
